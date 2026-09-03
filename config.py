6f7559b6ce7fe69d7609fb1e303c22e24664cb1e
Yusupov <sardor.0630.yusupov@gmail.com>
2026-09-03 22:04:29 +0500

    Fix admin ID

--- config.py
+++ config.py
@@ -1,27 +1,24 @@
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 ADMIN_ID = os.getenv("ADMIN_ID")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN topilmadi!")
-
 
 if not CHANNEL_ID:
     raise ValueError("CHANNEL_ID topilmadi!")
 
-
 if not ADMIN_ID:
     raise ValueError("ADMIN_ID topilmadi!")
 
-
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY topilmadi!")
+
+# Telegram ID raqamga aylantiriladi
+ADMIN_ID = int(ADMIN_ID)
